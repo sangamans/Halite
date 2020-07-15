cdc0b3c3e2386f6a954e10103de8bcf5487efdee
Sangaman Senthil <sangaman_senthil@tamu.edu>
2020-07-15 15:25:07 -0500

    new updates

--- submission/submission.py
+++ submission/submission.py
@@ -4,21 +4,26 @@ from random import choice
 
 # Create environment
 env = make("halite", configuration={ "episodeSteps": 400 }, debug=True)
+agent_count = 4
 print(env.configuration)
 
-# creating the agent
-def agent(obs,config):
+# creating the agent that will act random choice
+def random_agent(obs,config):
     
     board = Board(obs,config)
     me = board.current_player
     
     # Set actions for each ship
     for ship in me.ships:
-        ship.next_action = choice([ShipAction.NORTH,ShipAction.EAST,ShipAction.SOUTH,ShipAction.WEST,None])
+        ship.next_action = choice([ShipAction.NORTH,ShipAction.EAST,ShipAction.SOUTH,ShipAction.WEST,ShipAction.CONVERT,None])
     
     # Set actions for each shipyard
     for shipyard in me.shipyards:
-        shipyard.next_action = None
+        shipyard.next_action = choice([ShipyardAction.SPAWN,None])
     
     return me.next_actions
 
+
+env.reset(agent_count)
+env.run([random_agent, "random", "random", "random"])
+env.render(mode="ipython", width=500, height=450)
