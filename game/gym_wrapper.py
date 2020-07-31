986911deebe1f5b8cc008a24e4c74d68091fffe9
Sangaman Senthil <sangaman_senthil@tamu.edu>
2020-07-31 12:20:24 -0500

    committed changes

---
+++ game/gym_wrapper.py
@@ -0,0 +1,18 @@
+import numpy as np
+from kaggle_environments import make
+from kaggle_environments.envs.halite.helpers import *
+
+# to apply min-max scaling of observations
+class ScalingObservationWrapper(gym.ObservationWrapper):
+    def __init__(self, env, low=None, high=None):
+        super().__init__(env)
+        assert isinstance(env.observation_space, gym.spaces.Box)
+
+        low = np.array(self.observation_space.low if low is None else low)
+        high = np.array(self.observation_space.high if high is None else high)
+
+        self.mean = (high + low) / 2
+        self.max = high - self.mean
+
+    def observation(self, observation):
+        return (observation - self.mean) / self.max
